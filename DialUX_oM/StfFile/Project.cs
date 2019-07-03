2c4fc7c50b3a62ff69a04650a9f30244bed48157
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-07-03 14:25:38 +0100

    #1 - initial commit of oM, Engine, and Adapter

---
+++ DialUX_oM/StfFile/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+
+namespace BH.oM.DialUX
+{
+    public class Project : BHoMObject
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Planer { get; set; } = "00000";
+        public string Description { get; set; } = "BHoM Created STF File";
+        public List<Room> Rooms { get; set; } = new List<Room>();
+    }
+}
