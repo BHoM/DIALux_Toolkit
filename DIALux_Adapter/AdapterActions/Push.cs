f84c47ef0fb5f1cb075a7d8039f1e36d9a348997
Maja Lindroth <maja.lindroth@burohappold.com>
2020-01-02 16:53:49 +0000

    Adapter refactoring, not compiling

---
+++ DIALux_Adapter/AdapterActions/Push.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Data.Requests;
+using BH.oM.Adapter;
+using BH.oM.Base;
+using System.Reflection;
+
+namespace BH.Adapter.DIALux
+{
+    public partial class DialUXAdapter : BHoMAdapter
+    {
+        public override List<object> Push(IEnumerable<object> objects, string tag = "", PushType pushType = PushType.AdapterDefault, ActionConfig actionConfig = null)
+        {
+            // If unset, set the pushType to AdapterSettings' value (base AdapterSettings default is FullCRUD).
+
+            if (pushType == PushType.AdapterDefault)
+                pushType = m_AdapterSettings.DefaultPushType;
+
+
+            IEnumerable<IBHoMObject> objectToPush = ProcessObjectsForPush(objects, actionConfig); // Note: default Push only supports IBHoMObjects.
+
+            bool success = true;
+
+            MethodInfo methodInfos = typeof(Enumerable).GetMethod("Cast");
+            foreach (var typeGroup in objects.GroupBy(x => x.GetType()))
+            {
+                MethodInfo mInfo = methodInfos.MakeGenericMethod(new[] { typeGroup.Key });
+                var list = mInfo.Invoke(typeGroup, new object[] { typeGroup });
+                success &= ICreate(list as dynamic);
+            }
+
+            return success ? objects.ToList() : new List<IObject>();
+        }
+    }
+}
